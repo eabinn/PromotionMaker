2cb7d44826ad5daac56cee79c5ef708f49a9f611
inbaeyoonkr <iby2455@gmail.com>
2022-04-08 15:26:14 +0900

    Modify editor actions style

--- src/components/MakerEditor/EditorActions.vue
+++ src/components/MakerEditor/EditorActions.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="editor-actions">
-    <div>
-      <router-link to="/preview">프리뷰 보기</router-link>
-      <button @click="props.getResult($event)">에디터 결과</button>
+    <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 10px">
+      <router-link to="/preview" class="btn btn-warning">프리뷰 보기</router-link>
+      <button class="btn btn-default" @click="props.getResult($event)">에디터 결과</button>
     </div>
     <button class="btn btn-primary" @click="props.addSection">섹션 추가</button>
   </div>
